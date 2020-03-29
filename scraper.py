32462b2d8730e46d195727a71ec6dab918735e99
TijmenKort <54816563+TijmenKort@users.noreply.github.com>
2020-03-29 17:47:35 +0200

    script voor huis selecter toegevoegd

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 url = "https://www.woonnet.nl/huurwoning/amsterdam"
@@ -29,3 +30,17 @@ for house in houses_per_page:
     house_dict[url] = [street, zipcode, sq, rooms, gestoffeerd, available]
 
 print(house_dict)
+
+
+
+# Select options from house_dict
+zipcode_dict = json.loads(open('Postcode_damsco.txt', 'r').read())
+
+
+options = {house[0]: house[1] for house in house_dict.items() 
+               if house[1][1][:4] in zipcode_dict 
+               and int(house[1][2][:-3]) >= 50 
+               and int(house[1][3]) >= 2}
+
+
+print(options)
