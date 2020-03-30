78876f0b954a893a6ba48c227f1f7fbf0968ec9e
Timthijssen <47350186+Timthijssen@users.noreply.github.com>
2020-03-30 11:27:16 +0200

    Resulting csv's

--- functions_for_scraper.py
+++ functions_for_scraper.py
@@ -1,14 +1,15 @@
 import json
+import requests
 
-def options_creator(house_dict)
+def options_creator(house_dict):
     zipcode_dict = json.loads(open('Postcode_damsco.txt', 'r').read())
 
-    options = {key: house_info for key, house_info in house_dict.items() 
-                if house_info[1][:4] in zipcode_dict 
-                and int(house_info[2][1:].replace(',-', '')) <= 1600
-                and int(house_info[3][:-3]) >= 50 
-                and int(house_info[4]) >= 2}
-
+    options = {key: house_info for key, house_info in house_dict.items()
+            if house_info[1][:4] in zipcode_dict 
+            and float((house_info[2][1:])) <= 1600.0
+            and int(house_info[3]) >= 50
+            and int(house_info[4]) >= 2}
+    
     return options
 
         
@@ -16,7 +17,7 @@ def zipcode_search(streetname):
     
     streetname= streetname.strip()
     streetname = streetname.replace(' ', '+')
-    streetname += '+Amsterdam'
+    streetname += '+1+Amsterdam'
     
     # Google Geocoding API
     input = 'https://maps.googleapis.com/maps/api/geocode/json?address='+streetname+'&key=AIzaSyBAVfjRxqU9ynAFuq_DQ_JWRbSgI0Jy0gw'
@@ -25,6 +26,9 @@ def zipcode_search(streetname):
     r = requests.get(input)
     data = r.json()
 
-    return data['results'][0]['address_components'][6]['long_name'][:4]
-
+    
+    try: 
+        return data['results'][0]['address_components'][-1]['long_name']
+    except:
+        return 'onbekend'
            
